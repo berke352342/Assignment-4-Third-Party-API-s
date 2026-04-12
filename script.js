71f9002478fba3f804dfbd37e754fd2719be71a2
Shakhboz Soliev <shakhbozsoliev60@gmail.com>
2026-04-12 12:36:05 -0400

    Shakhboz: added IMDB rating and enter key

--- script.js
+++ script.js
@@ -23,6 +23,7 @@
                      <p><strong>Year:</strong> ${data.Year}</p>
                      <p><strong>Genre:</strong> ${data.Genre}</p>
                      <p><strong>Director:</strong> ${data.Director}</p>
+                     <p><strong>IMDB Rating:</strong> ${data.imdbRating}</p>
                      <p><strong>Plot:</strong> ${data.Plot}</p>
                      <img src="${data.Poster}" alt="${data.Title} Poster">
                 `;
